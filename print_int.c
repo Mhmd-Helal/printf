a9a82fe12e53e4caf6641ab624c8bb445481cc80
mhmd-helal <mhmd.mhmd.helal.com>
2023-11-14 03:21:00 -0800

    ans

--- print_int.c
+++ print_int.c
@@ -2,33 +2,82 @@
 /**
  *print_i - print intger
  *@args: arguments
- *Return: 2
+ *Return: len of num
  */
 int print_i(va_list args)
 {
-	int n, last, num;
+	int n, nd, last, exp = 1;
+	int digit;
+	int i = 1;
 
 	n = (va_arg(args, int));
 	last = n % 10;
-	num = n / 10;
-	_putchar(num + '0');
+	nd = n / 10;
+	if (last < 0)
+	{
+		_putchar('-');
+		nd = -nd;
+		last = -last;
+		i++;
+	}
+	if (nd > 0)
+	{
+		while (nd / 10 != 0)
+		{
+			exp = exp * 10;
+			nd = nd / 10;
+		}
+		nd = n / 10;
+		while (exp > 0)
+		{
+			digit = nd / exp;
+			_putchar(digit + '0');
+			nd = nd - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
 	_putchar(last + '0');
-	return (2);
+	return (i);
 }
 /**
- *print_i - print intger
+ *print_d - print intger
  *@args: arguments
- *Return: 2
+ *Return: len of num
  */
 int print_d(va_list args)
 {
-	int n, last, num;
+	int n, nd, last, exp = 1;
+	int digit;
+	int i = 1;
 
 	n = (va_arg(args, int));
 	last = n % 10;
-	num = n / 10;
-	_putchar(num + '0');
+	nd = n / 10;
+	if (last < 0)
+	{
+		_putchar('-');
+		nd = -nd;
+		last = -last;
+		i++;
+	}
+	if (nd > 0)
+	{
+		while (nd / 10 != 0)
+	{
+		exp = exp * 10;
+		nd = nd / 10;
+	}
+	nd = n / 10;
+	while (exp > 0)
+	{
+		digit = nd / exp;
+		_putchar(digit + '0');
+		nd = nd - (digit * exp);
+		exp = exp / 10;
+		i++;
+	}
+	}
 	_putchar(last + '0');
-	return (2);
+	return (i);
 }
-
