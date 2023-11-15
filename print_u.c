566a77ff086008a24772ec8b8ade7bc78ca63c0c
mhmd-helal <mhmd.mhmd.helal.com>
2023-11-15 03:33:03 -0800

    ans

--- print_u.c
+++ print_u.c
@@ -4,7 +4,7 @@
  *@args: arguments
  *Return: len of num
  */
-int print_i(va_list args)
+int print_u(va_list args)
 {
 	unsigned int n, num, last, exp = 1;
 	int digit;
@@ -30,49 +30,6 @@ int print_i(va_list args)
 			exp = exp / 10;
 			i++;
 		}
-	}
-	_putchar(last + '0');
-	return (i);
-}
-/**
- *print_d - print intger
- *@args: arguments
- *Return: len of num
- */
-int print_d(va_list args)
-{
-	int n, num, last, exp = 1;
-	int digit;
-	int i = 1;
-
-	n = (va_arg(args, int));
-	last = n % 10;
-	n = n / 10;
-	num = n;
-	if (last < 0)
-	{
-		_putchar('-');
-		n = -n;
-		num = -num;
-		last = -last;
-		i++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-	{
-		exp = exp * 10;
-		num = num / 10;
-	}
-	num = n;
-	while (exp > 0)
-	{
-		digit = num / exp;
-		_putchar(digit + '0');
-		num = num - (digit * exp);
-		exp = exp / 10;
-		i++;
-	}
 	}
 	_putchar(last + '0');
 	return (i);
