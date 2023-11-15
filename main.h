551adb33bc567e8b5f328301f56c2682fec43d55
mhmd-helal <mhmd.mhmd.helal.com>
2023-11-15 05:53:04 -0800

    ans

--- main.h
+++ main.h
@@ -18,6 +18,7 @@ int print_b(va_list args);
 int print_u(va_list args);
 int print_o(va_list args);
 int print_x(va_list args);
+int print_X(va_list args);
 int _printf(const char *format, ...);
 typedef struct format
 {
